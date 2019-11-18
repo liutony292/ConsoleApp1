a10f3408c16f5629cdc5fa1998f51b4264082019
Tony <liutony292@gmail.com>
2019-11-17 16:34:30 -0800

    C2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Test");
+            Console.ReadKey();
+            Console.WriteLine("Test");
+            Console.ReadKey();
+
         }
     }
 }
